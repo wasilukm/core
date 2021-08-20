20194b857d01440d690cf3f4f8c6de6e83437e78
Paulus Schoutsen <balloob@gmail.com>
2021-08-20 09:04:00 -0700

    Allow confirming local push notifications

--- homeassistant/components/websocket_api/http.py
+++ homeassistant/components/websocket_api/http.py
@@ -230,7 +230,7 @@ class WebSocketHandler:
             unsub_stop()
 
             if connection is not None:
-                connection.async_close()
+                connection.async_handle_close()
 
             try:
                 self._to_write.put_nowait(None)
